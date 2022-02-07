3c665d2cb988f78840097255cfad62428bfcc04e
georgedmendoza <georgedmendoza13@gmail.com>
2022-02-07 10:41:32 -0600

    updated vs

---
+++ student-admin-portal-api/student-admin-portal-api/DataModels/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace student_admin_portal_api.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        // Nav prop
+        public Guid StudentId { get; set; }
+    }
+}
---
+++ student-admin-portal-api/student-admin-portal-api/DataModels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace student_admin_portal_api.DataModels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
